6e4e5b040f14a60ad5cd2313403cc6a08f38b8d1
David McReynolds <dave@thedaylightstudio.com>
2012-04-01 23:30:38 -0700

    user guide documentation for google_keywords

--- views/_docs/index.php
+++ views/_docs/index.php
@@ -1,5 +1,9 @@
-<h1>Google Keywords Documentation</h1>
+<h1>Google Keywords Module Documentation</h1>
+<p>This Google Keywords module documentation is for version <?=GOOGLE_KEYWORDS_VERSION?>.</p>
+
+<h2>Overview</h2>
 <p>The Google Keywords module allows you to search a given domain with certain keywords to find out how highly you rank in the top 100.</p>
+
 <?=generate_config_info()?>
 
 <?=generate_toc()?>
