c5b2f927678d558dcec037acacfb3972ad7059da
David McReynolds <dave@bydaylight.com>
2016-04-04 21:28:10 -0700

    Changed case of controllers for CI 3

--- libraries/Fuel_google_keywords.php
+++ libraries/Fuel_google_keywords.php
@@ -69,7 +69,7 @@ class Fuel_google_keywords extends Fuel_advanced_module {
 	 * @param	array	config preferences
 	 * @return	void
 	 */	
-	function initialize($params)
+	function initialize($params = array())
 	{
 		parent::initialize($params);
 		$this->set_params($this->_config);
